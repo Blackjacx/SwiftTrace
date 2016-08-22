ff7a4198a18cc295e3666793ddd538a331ac48d0
Stefan Herold <stefan.herold@flincteam.org>
2016-08-22 23:51:30 +0200

    minor optimization

--- Sources/Sphere.swift
+++ Sources/Sphere.swift
@@ -13,6 +13,7 @@ public struct Sphere: Object {
     public var material: Material
     public var C: Point3
     public var r: Double
+    public var rr: Double
 
     /**
      * Returns a sphere from a json object
@@ -27,6 +28,7 @@ public struct Sphere: Object {
         self.material = try Material(json: material)
         self.C = try Point3(json: center)
         self.r = radius
+        self.rr = radius*radius
     }
 
 
@@ -37,20 +39,25 @@ public struct Sphere: Object {
         self.material = material
         self.C = center
         self.r = radius
+        self.rr = radius*radius
     }
 
+    /**
+     * Quadratic solution for sphere ray intersection.
+     *
+     */
     public func intersect(ray: Ray3, t: inout Double) -> Bool {
         let CO = ray.origin - C                 // vector from center to origin
         let p = 2.0 * CO.dot(v: ray.direction)  // part I for quadratic equation
-        let q = CO.dot(v: CO) - r*r             // part II for quadratic equation
-        let D = pow(p*0.5, 2.0) - q             // discriminant
+        let q = CO.dot(v: CO) - rr              // part II for quadratic equation
+        let D = (p*0.5) * (p*0.5) - q           // discriminant
 
         if D < 0.0 {
             return false                        // 0 solutions
         }
 
         // distance calculation
-        let t1 = (-p * 0.5) - sqrt(D)
+        let t1 = (-p*0.5) - sqrt(D)
 
         if t1 > DBL_EPSILON {                   // surely outside sphere (smallest ISP lies in ray direction)
             t = t1
